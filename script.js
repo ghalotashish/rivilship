a83c4f471ef765b4563b8652ed2bc062c6171668
ghalotashish <159156714+ghalotashish@users.noreply.github.com>
2024-02-07 21:15:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+function register() {
+    // Redirect to registration page
+    window.location.href = "register.html";
+}
+
+function login() {
+    // Redirect to login page
+    window.location.href = "login.html";
+}
